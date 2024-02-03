7547dba769f6860b546175dc259d9b3574838777
DanieleSeverin89 <daniele.severin@hbtsa.it>
2024-02-03 17:39:12 +0100

    removed logger

--- UrlShortener/Program.cs
+++ UrlShortener/Program.cs
@@ -12,29 +12,19 @@ builder.Logging.AddConsole();
 builder.Logging.AddDebug(); 
 builder.Logging.AddAzureWebAppDiagnostics();
 
-Logger.Log("Start");
-
 builder.Services.AddControllers();
-Logger.Log("AddControllers");
 builder.Services.AddEndpointsApiExplorer();
-Logger.Log("AddEndpointsApiExplorer");
 builder.Services.AddSwaggerGen();
-Logger.Log("AddSwaggerGen");
 
 builder.Configuration.AddEnvironmentVariables();
-Logger.Log("AddEnvironmentVariables");
 
-//var connectionString = builder.Configuration.GetConnectionString("Database") ??
-//            throw new ArgumentNullException(nameof(builder.Configuration));
+var connectionString = builder.Configuration.GetConnectionString("Database") ??
+            throw new ArgumentNullException(nameof(builder.Configuration));
 
-//Logger.Log("GetConnectionString");
-
-//builder.Services.AddDbContext<ApplicationDbContext>(options =>
-//{
-//    options.UseNpgsql(connectionString).UseSnakeCaseNamingConvention();
-//});
-
-//Logger.Log("AddDbContext");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseNpgsql(connectionString).UseSnakeCaseNamingConvention();
+});
 
 builder.Services.AddSingleton<UrlShorteningService>();
 builder.Services.AddSingleton<UniqueUrlCodeProvider>();
@@ -42,35 +32,21 @@ builder.Services.AddSingleton<UniqueUrlCodesPool>();
 builder.Services.AddSingleton<UrlCodeUsedEvent>();
 builder.Services.AddSingleton<UrlCodeGenerationSubscriber>();
 
-Logger.Log("AddSingleton");
-
 var app = builder.Build();
-
-Logger.Log("Build");
 
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
-Logger.Log("SetSwitch");
-
 // Trigger subscription
 var subscriber = app.Services.GetRequiredService<UrlCodeGenerationSubscriber>();
-
-Logger.Log("subscriber");
 
 // Configure the HTTP request pipeline.
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 
-Logger.Log("UseMiddleware");
-
 app.UseSwagger();
 app.UseSwaggerUI();
 
-Logger.Log("UseSwagger");
-
 //app.ApplyMigrations();
-
-//Logger.Log("ApplyMigrations");
 
 app.UseCors(builder =>
 {
@@ -79,10 +55,6 @@ app.UseCors(builder =>
             .AllowAnyHeader();
 });
 
-Logger.Log("UseCors");
-
 app.MapControllers();
-
-Logger.Log("MapControllers");
 
 app.Run();
